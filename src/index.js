ae2c803aba165195318e311b2803faf8db9b921b
Amanda McCabe <amccabe1316@gmail.com>
2020-08-24 14:41:16 -0600

    added updates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './App.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import STORE from './STORE';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App store={STORE} />,
   document.getElementById('root')
 );
 
